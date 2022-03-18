860ec7e277ab2d1fa01b2da697abd19a30ecb9c4
moddedmcplayer <moddedmcplayer@gmail.com>
2022-03-18 20:29:43 +0100

    stuff

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Interfaces;
+
+namespace elevatorControl
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        
+        //[Description("How long an elevator takes in seconds")]
+        //public float elevatorTime =
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API;
+using Exiled.API.Features;
+using Exiled.Events.Handlers;
+
+namespace elevatorControl
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "moddedmcplayer";
+        public override string Name { get; } = "elevatorControl";
+        public override Version Version { get; } = new Version(0, 0, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
+
+        public override void OnEnabled()
+        {
+            RegisterEvents(); 
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            UnRegisterEvents();
+            base.OnDisabled();
+        }
+
+        private void RegisterEvents()
+        {
+
+        }
+
+        private void UnRegisterEvents()
+        {
+
+        }
+    }
+}
